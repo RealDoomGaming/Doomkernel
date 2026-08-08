92409866f5e7eba11837d9fedd9a4a12da642634
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-08 18:09:01 +0200

    started working on a string header file and removed unnescesarry stuff in the stdio header file

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -7,17 +7,8 @@
 // EOF gets returned when any of the 3 functions fails so we need to define it here
 #define EOF (-1)
 
-// we need to do this here because if we ever do anything with c++ it should be able to call these and if we dont have this here then it can see them but wont find the functions
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int printf(const char*, ...);
 int putchar(int);
 int puts(const char*);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
---
+++ libc/include/string.h
@@ -0,0 +1,10 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <sys/cdefs.h>
+#include <stddef.h>
+
+// for now I only need the strlen one
+size_t strlen(const char*);
+
+#endif
