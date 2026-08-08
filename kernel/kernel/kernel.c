26c4a5a7c8141109e3a7fcb14d6d6c2df59729ea
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-08 15:44:33 +0200

    changed the kernel so for no it only inits the terminal which also only clears the terminal and setts some stuff

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,27 +2,7 @@
 
 #include <kernel/tty.h>
 
-// we need to use the uint16_t* type here since for vga every character is stored as 16bit and not like normally as 8bit
-#define VGA ((volatile uint16_t*) 0xB8000)
-// width and height of the screen in characters
-#define VGA_WIDTH 80
-#define VGA_HEIGHT 25
-// color
-#define WHITE_ON_BLACK 0x0F
-
-// we need a global cursor position state
-static int cursor_x = 0;
-static int cursor_y = 0;
-
-void clear_screen() {
-    // here we combine the char and the color of the char into a 16 bit entry which we will use to clear the screen later
-    uint16_t blank = ' ' | (WHITE_ON_BLACK << 8);
-
-    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
-        VGA[i] = blank;
-    }
-}
-
+/*
 void put_char(const char c) {
     // when we want to print a char we firstly need to check if its a newline or a carriage return
     // and only then can we combine the character and the color into a 16 bit entry and put it inot the VGA buffer
@@ -56,12 +36,13 @@ void print(const char msg[], const int msg_length) {
     for (int i = 0; i < msg_length; i++) {
         put_char(msg[i]);
     } 
-}
+}*/
 
 void kernel_main() {
-    // first thing we do is clear the screen of any leftover text
-    clear_screen();
+    // first thing we do is init the terminal
+    terminal_init();
 
+    /*
     // this is our success message string
     const char success_msg[] = "Successfully loaded the kernel. \n";
     const int succ_msg_length = 33;
@@ -73,6 +54,7 @@ void kernel_main() {
     const int cursor_test_msg_length = 34;
 
     print(cursor_test_msg, cursor_test_msg_length);
+    */
 
     // while loop so the cpu doesnt run off into memory junk
     while (1) {
