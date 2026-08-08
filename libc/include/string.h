f8850628306e88942daba8cbf548ac3846081439
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-08 20:42:23 +0200

    modified the header file so I have more string functions and also made the files with nothing in them yet

--- libc/include/string.h
+++ libc/include/string.h
@@ -4,7 +4,10 @@
 #include <sys/cdefs.h>
 #include <stddef.h>
 
-// for now I only need the strlen one
+int memcmp(const char*, const char*, size_t);
+void* memcpy(void*, const void*, size_t);
+void* memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
 size_t strlen(const char*);
 
 #endif
