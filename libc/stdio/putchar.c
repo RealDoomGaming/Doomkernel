4e05d55a6f0ffc83960dd6f7c147283c1eb93840
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-08 17:53:50 +0200

    made another comment in the putchar file

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -4,6 +4,7 @@
 // this is for writing one character to the terminal
 int putchar(int int_char) {
     char c = (char) int_char;
+    // this just calls the function from the tty.h file which outputs to the vga
     terminal_write(&c, sizeof(c));
 
     return int_char;
