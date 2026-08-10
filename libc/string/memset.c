3a07a94d6d936082019f8869b23394784d1ba1ba
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-10 12:05:42 +0200

    made the memset function

---
+++ libc/string/memset.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+void* memset(void* destptr, int value, size_t size) {
+    unsigned char* dest = (unsigned char*) destptr;
+
+    for (size_t i = 0, i < size, i++) {
+        dest[i] = (unsigned char*) value;
+    }
+
+    return destptr;
+}
