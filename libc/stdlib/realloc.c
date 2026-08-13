4cf7d534a6c651ad2949dd203f95b11c897174c3
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-13 12:41:18 +0200

    expanded the realloc function so now when we want to make a memory chunck smaller it splits into 2 memory chuncks

--- libc/stdlib/realloc.c
+++ libc/stdlib/realloc.c
@@ -20,7 +20,22 @@ void* realloc(void* ptr, size_t size) {
 
     // then we want to see if the old size is bigger then the new size
     if (alloc_cur->size >= size) {
-        // here we could actually shrink the memory but this is the lazy approach
+        // if we want to shrink the memory we firstly have to see if it is even worth it because we have
+        // to split the memory chunck into 2
+
+        if (alloc_cur->size - size > sizeof(alloc_t)) {
+            // make a new memory chunck here
+            alloc_t *remainder = (alloc_t *)((uint8_t *)alloc_cur + sizeof(alloc_t) + size);
+            // then we assign it everything
+            remainder->status = 0;
+            remainder->size = alloc_cur->size - size - sizeof(alloc_t);
+
+            // then we set the size of the memory chunck which we wanted to reduce to the reduced size
+            alloc_cur->size = size;
+        }
+
+
+        // and return
         return ptr;
     }
 
