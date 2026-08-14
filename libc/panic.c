933598c798933e6a644c9341769c487340bd3912
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-14 17:16:49 +0200

    fixxed something in the panic

--- libc/panic.c
+++ libc/panic.c
@@ -4,7 +4,7 @@ void panic(const char* format, const char* file, const int line) {
     char *panic_msg = "****KERNEL PANIC**** \n At file: %s. \n";
 
     printf(panic_msg, file);
-    printf(format);
+    printf("%s\n", format);
 
     for (;;) __asm__ volatile ("cli; hlt");
 }
