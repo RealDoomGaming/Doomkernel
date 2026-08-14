ad684a3781bf54ccebf6c101af8d05989b6ee846
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-14 20:33:13 +0200

    finished the pic remap function and changed up some stuff in the header file like removing the parameter names

--- kernel/include/interrupts/pic.h
+++ kernel/include/interrupts/pic.h
@@ -5,10 +5,10 @@
 
 // PIC stands for Programmable interface controller and we manage hardware interrupts with it
 // we need a few functions
-void pic_remap(int offset_1, int offset_2); // pic remap is needed to reconfigure the master and slave 8259 chips to send custom interupt signals
-void pic_send_eoi(uint8_t irq);             // this send and eoi (End of interupt) signal back to the PIC chips
-void pic_set_mask(uint8_t irq);             // this disables (or masks however you wanna call it) a specific IRQ
-void pic_clear_ask(uint8_t irq);            // and this enables (unmasks) a specific IRQ
+void pic_remap(int, int); // pic remap is needed to reconfigure the master and slave 8259 chips to send custom interupt signals
+void pic_send_eoi(uint8_t);             // this send and eoi (End of interupt) signal back to the PIC chips
+void pic_set_mask(uint8_t);             // this disables (or masks however you wanna call it) a specific IRQ
+void pic_clear_ask(uint8_t);            // and this enables (unmasks) a specific IRQ
 
 
 #endif
--- kernel/interrupts/pic.c
+++ kernel/interrupts/pic.c
@@ -32,3 +32,47 @@ static inline void io_wait() {
     // so writing a dummy byte to port 0x80 forces the cpu to pause for a second
     outb(0x80, 0);    
 }
+
+// now we go to the actual function in this file
+void pic_remap(int offset1, int offset2) {
+    // the problem is that on modern processors the first 31 vectors are reserved for CPU Exceptions
+    // but that created a conflict with IBMS Pc hardware design so we have to remap here
+
+    // firstly we save the existing state
+    uint8_t mask1 = inb(PIC1_DATA);
+    uint8_t mask2 = inb(PIC2_DATA);
+
+    // then we start the inilization and send 0x11 (ICW1_INIT | ICW1_ICW4) to both command ports
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+
+    // then after that we have to set the base offset vectors
+    // so irq 0 maps to  offset1 + 0
+    // irq1 maps to offset1 + 1
+    // irq8 maps to offset2 + 0
+    // and so on...
+    outb(PIC1_DATA, offset1);
+    io_wait();
+    outb(PIC2_DATA, offset2);
+    io_wait();
+
+    // after that we configure cascading by
+    // making the master pic receive bitmask 4 which tells it that irq 2 is connected to the slave pic
+    outb(PIC1_DATA, 4);
+    io_wait();
+    // and telling the slave pic that its cascade identity is irq 2
+    outb(PIC2_DATA, 2);
+    io_wait();
+
+    // and we also tell both pics to operate in 8086/88 architecture mode
+    outb(PIC1_DATA, ICW4_8086);
+    io_wait();
+    outb(PIC2_DATA, ICW4_8086);
+    io_wait();
+
+    // and finally we restore what we saved from the pics before
+    outb(PIC1_DATA, mask1);
+    outb(PIC2_DATA, mask2);
+}
