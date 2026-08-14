b33dc30a7f897a3ef1d77132f9181ca39bd94ab9
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-14 13:25:51 +0200

    made the basic struct for all the items in the idt

---
+++ kernel/interrupts/idt.h
@@ -0,0 +1,17 @@
+#ifndef _IDT_H
+#define _IDT_H
+
+#include <stdint.h>
+
+// this will be each item of the interrupter descriptor table (idt)
+typedef struct {
+    uint16_t offset_1;          // this is the first offset in the item
+    uint16_t segment_selector;  // this is the segment selector in the item (must point to a valid code item in the gdt)
+    uint8_t ist;                // holds the Interrupter Stack table offset 
+    uint8_t type;               // this holds the gate type, dpl and p fields in the item
+    uint16_t offset_2;          // second offset in the item
+    uint32_t offset_3;          // third offset in the item
+    uint32_t zero;              // reserved bits in the item
+} interrupter_descriptor;
+
+#endif
