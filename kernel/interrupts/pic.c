8df17a7dfa42396f41cb0424362c21d3bb0ca82a
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-14 20:42:34 +0200

    finished the pic c file

--- kernel/interrupts/pic.c
+++ kernel/interrupts/pic.c
@@ -5,10 +5,10 @@
 // these hold parameters, interrupt masks and vector offsets
 #define PIC1_COMMAND 0x20
 #define PIC1_DATA 0x21
-#define PIC2_COMMANDS 0xA0
+#define PIC2_COMMAND 0xA0
 #define PIC2_DATA 0xA1
 
-#define PIC1_EOI 0x20
+#define PIC_EOI 0x20
 #define ICW1_INIT 0x10      // this puts the PIC into init mode
 #define ICW1_ICW4 0x01      // this signals that a 4th init command will follow
 #define ICW4_8086 0x01      // this tells the PIC to operate in x86/8086 mode
@@ -75,4 +75,31 @@ void pic_remap(int offset1, int offset2) {
     // and finally we restore what we saved from the pics before
     outb(PIC1_DATA, mask1);
     outb(PIC2_DATA, mask2);
+}
+
+void pic_send_eoi(uint8_t irq) {
+    // this function gets called when a device triggers and interrupt
+    // and it is needed because we need to send and eoi command else the pci thinks the interrupt is still running and will freeze everything else
+    if (irq >= 8) {
+        outb(PIC2_COMMAND, PIC_EOI);
+    }
+
+    outb(PIC1_COMMAND, PIC_EOI);
+}
+
+void pic_set_mask(uint8_t irq) {
+    // this enables the mask (disables the irq)
+    uint16_t port = irq < 8 ? PIC1_DATA : PIC2_DATA;     // so we know if its a slave or master
+    uint8_t irq_line = irq < 8 ? irq : irq - 8;          // converts full irq numbers to chip irq indexes
+    uint8_t value = inb(port) | (1 << irq_line);         // and this reads the current register and sets the corresponding bit with the OR operation
+    outb(value, port);                                   // and this finally writes updated masks
+}
+
+void pic_clear_mask(uint8_t irq) {
+    // this basically the same as setting the mask the only difference is
+    uint16_t port = irq < 8 ? PIC1_DATA : PIC2_DATA;
+    uint8_t irq_line = irq < 8 ? irq : irq - 8;
+    // here where we clear the target with the AND
+    uint8_t value = inb(port) & ~(1 << irq_line);
+    outb(port, value);
 }
