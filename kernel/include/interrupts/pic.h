a6e55ebf68f280011685d0cde13d5874b867fb72
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-16 14:30:13 +0200

    fixed a typo

--- kernel/include/interrupts/pic.h
+++ kernel/include/interrupts/pic.h
@@ -8,7 +8,7 @@
 void pic_remap(int, int); // pic remap is needed to reconfigure the master and slave 8259 chips to send custom interupt signals
 void pic_send_eoi(uint8_t);             // this send and eoi (End of interupt) signal back to the PIC chips
 void pic_set_mask(uint8_t);             // this disables (or masks however you wanna call it) a specific IRQ
-void pic_clear_ask(uint8_t);            // and this enables (unmasks) a specific IRQ
+void pic_clear_mask(uint8_t);            // and this enables (unmasks) a specific IRQ
 
 
 #endif
