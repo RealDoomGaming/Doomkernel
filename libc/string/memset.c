86f115581ef50e547e8fb064d4fe0c68a9eb365a
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-16 14:24:47 +0200

    memset wrote 1 byte too many so I fixxed that

--- libc/string/memset.c
+++ libc/string/memset.c
@@ -3,7 +3,7 @@
 void memset(void* destptr, int value, size_t size) {
     unsigned char* dest = (unsigned char*) destptr;
 
-    for (size_t i = 0; i <= size; i++) {
+    for (size_t i = 0; i < size; i++) {
         dest[i] = (unsigned char) value;
     }
 }
