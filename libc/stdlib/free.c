7ffdeadd8b594e303d569f0fc898b98f54ccbad2
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-18 16:36:14 +0200

    added merging to the memory blocks when freeing them

--- libc/stdlib/free.c
+++ libc/stdlib/free.c
@@ -2,7 +2,25 @@
 #include <string.h>
 #include <stddef.h>
 
-void free (void* ptr) {
+alloc_t* find_before(alloc_t *alloc) {
+    // in this function we basically start at the heaps beginning and move up until we find that the next alloc is the one we got as a parameter
+
+    alloc_t *cur = (alloc_t*)((uint8_t*)heap_beginning);
+    alloc_t *next = (alloc_t*)((uint8_t*)cur + sizeof(alloc_t) + cur->size);
+
+    if (cur == alloc) {
+        return 0;
+    }
+
+    while (next != alloc) {
+        cur = next;
+        next = (alloc_t*)((uint8_t*)cur + sizeof(alloc_t) + cur->size);
+    }
+
+    return cur;
+}
+
+void free(void* ptr) {
     // in free we get a ptr to a memory address
     // now since we have our handy allocate struct infront of each allocated memory block we can just subtract the sizeof the struct and get the struct
     // then we can set it to free and also clear the memory which was used
@@ -20,6 +38,23 @@ void free (void* ptr) {
     alloc->status = 0;
 
     memory_used -= sizeof(alloc_t) + alloc->size;
+
+    // after we have set the status of the memory chunck to unused we can check if there are any 
+    // adjacent memory chuncks which are also unused and merge them if they are
+    // the next memory chunk
+    alloc_t *next = (alloc_t*)((uint8_t*)ptr + alloc->size);
+    alloc_t *before = find_before(alloc);
+
+    // firstly add the next one
+    if ((uint64_t)next < last_alloc && next->status == 0) {
+        alloc->size += sizeof(alloc_t) + next->size;
+    }
+
+    // and then we merge the whole block backwards if the one before is free
+    if (before && before->status == 0) {
+        before->size += sizeof(alloc_t) + alloc->size;
+        alloc = before;
+    }
 
     memset((void*)((uint64_t)alloc + sizeof(alloc_t)), 0, alloc->size);
 }
